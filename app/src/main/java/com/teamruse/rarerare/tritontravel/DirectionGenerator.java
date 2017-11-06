efcd35f399e623b65e5858824cad4d530a012d78
x6wu <x6wu@ucsd.edu>
2017-11-06 14:45:03 -0800

    api request in progress

--- app/src/main/java/com/teamruse/rarerare/tritontravel/DirectionGenerator.java
+++ app/src/main/java/com/teamruse/rarerare/tritontravel/DirectionGenerator.java
@@ -8,6 +8,7 @@ import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -28,25 +29,52 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class Direction {
+
+public class DirectionGenerator {
     private String origin;
     private String dest;
+    private DirectionGeneratorListener listener;
     private static final String ApiUrl = "https://maps.googleapis.com/maps/api/directions/json?";
     private static final String ApiKey = "AIzaSyDL3mGK2R6RvVHzHqhz7f2623iv6gGfU9w";
 
-    public Direction(String origin, String dest){
+    public DirectionGenerator(DirectionGeneratorListener listener, String origin, String dest){
+        this.listener = listener;
         this.origin = origin;
         this.dest = dest;
     }
 
-    public String UrlBuilder(){
+    public String buildUrl(){
         String originUrl = origin.replaceAll(" ", "+");
         String destUrl = dest.replaceAll(" ", "+");
         return ApiUrl + "origin=" + originUrl + "&destination="
                 + destUrl + "&mode=transit&key=" + ApiKey;
-
     }
 
+    public void generate() {
+        listener.onStart();
+        //TODO
+    }
+
+    private class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                URL url = new URL(strings[0]);
+                InputStream in = url.openConnection().getInputStream();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 
 }
---
+++ app/src/main/java/com/teamruse/rarerare/tritontravel/DirectionGeneratorListener.java
@@ -0,0 +1,10 @@
+package com.teamruse.rarerare.tritontravel;
+
+/**
+ * Created by Xinyu on 11/6/2017.
+ */
+
+public interface DirectionGeneratorListener {
+    void onStart();
+    void onSuccess();
+}
