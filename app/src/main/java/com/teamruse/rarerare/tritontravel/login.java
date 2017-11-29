4bc0f317c6943b46c0e037be23015603aaf54a06
ceruleangu <cerulean.gu@gmail.com>
2017-11-29 12:54:00 -0800

    modify login, and delete signup and forget password page

--- app/src/main/java/com/teamruse/rarerare/tritontravel/login.java
+++ app/src/main/java/com/teamruse/rarerare/tritontravel/login.java
@@ -1,5 +1,7 @@
 package com.teamruse.rarerare.tritontravel;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,25 +11,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class login extends Fragment {
     View myView;
+    SignInButton googleButton;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         myView = inflater.inflate(R.layout.login, container, false);
+
         defineButtons(myView);
+
         return myView;
     }
 
     public void defineButtons(View view) {
-        view.findViewById(R.id.signup).setOnClickListener(buttonClickListener);
-        view.findViewById(R.id.forget).setOnClickListener(buttonClickListener);
         view.findViewById(R.id.back).setOnClickListener(buttonClickListener);
-        view.findViewById(R.id.login).setOnClickListener(buttonClickListener);
+
+        googleButton = view.findViewById(R.id.login);
+        googleButton.setOnClickListener(buttonClickListener);
+
+        //googleButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
     }
 
 
@@ -36,67 +48,16 @@ public class login extends Fragment {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v){
-                switch (v.getId()) {
-                    case R.id.signup:
-                        //startActivity(new Intent(login.this, signup.class));
-                        getFragmentManager().beginTransaction().replace(R.id.login_view, new signup())
-                                .commit();
-                        break;
+                if (v.getId() == R.id.back) {
+                    getFragmentManager().beginTransaction().replace(R.id.login_view, new MapFragment())
+                            .commit();
+                }
 
-                    case R.id.forget:
-                        getFragmentManager().beginTransaction().replace(R.id.login_view, new forget())
-                                .commit();
 
-                        break;
-
-                    case R.id.back:
-                        getFragmentManager().beginTransaction().replace(R.id.login_view, new MapFragment())
-                                .commit();
-
-                        break;
-
-                    case R.id.login:
-                        send_click(v);
-                        break;
-            }
         }
     };
 
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void send_click (View v) {
 
-        EditText msg = (EditText) myView.findViewById(R.id.email);
-        EditText psw = (EditText) myView.findViewById(R.id.password);
-
-        if (msg.getText().toString().equals("")) {
-            msg.setError("Required");
-        }
-
-        else if (psw.getText().toString().equals("")) {
-            psw.setError("Required");
-        }
-
-
-        else if (!isEmailValid(msg.getText().toString())) {
-            msg.setError("Invalid email");
-        }
-
-        //data base check
-
-        else {
-            getFragmentManager().beginTransaction().replace(R.id.login_view, new MapFragment())
-                    .commit();
-        }
-
-
-    }
-
-    public static boolean isEmailValid(String email) {
-        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
-        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
 
 }
